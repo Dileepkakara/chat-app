ad843c6270284d8fb1f35b694327cc7d60b6e132
dileepkakara <dileepkakara20@gmail.com>
2025-09-12 21:14:55 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+//const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.router.js";
+dotenv.config();
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(3000, () => console.log("Server is running on port:" + PORT));
