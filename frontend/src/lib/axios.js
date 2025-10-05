97512c3a6f3ea03d23d42b6741cc19425ac5c03c
dileepkakara <dileepkakara20@gmail.com>
2025-10-05 16:37:08 +0530

    Fix axios baseURL to connect to Render backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
     baseURL:
         import.meta.env.MODE === "development"
             ? "http://localhost:3000/api" // local backend
-            : "https://chat-backend-3-m788.onrender.com/api", // Render backend
+            : "https://chat-backend-3-m788.onrender.com", // Render backend
     withCredentials: true,
 });
